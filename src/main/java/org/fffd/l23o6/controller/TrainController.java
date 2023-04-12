9931052c837db6fa41bca0e5b2f24f963895dc0e
Bob Huang <bc3104hzy@126.com>
2023-04-12 22:52:02 +0800

    Add TrainService. (WIP)

--- src/main/java/org/fffd/l23o6/controller/TrainController.java
+++ src/main/java/org/fffd/l23o6/controller/TrainController.java
@@ -2,10 +2,10 @@ package org.fffd.l23o6.controller;
 
 import java.util.Date;
 
+import org.fffd.l23o6.pojo.vo.PagedResult;
 import org.fffd.l23o6.pojo.vo.Response;
 import org.fffd.l23o6.pojo.vo.train.ListTrainRequest;
 import org.fffd.l23o6.pojo.vo.train.TrainVO;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +18,12 @@ import jakarta.validation.Valid;
 @RequestMapping("/v1/")
 public class TrainController {
     @GetMapping("train")
-    public Response<Page<TrainVO>> listTrains(@Valid @RequestParam ListTrainRequest request) {
+    public Response<PagedResult<TrainVO>> listTrains(@Valid @RequestParam ListTrainRequest request) {
         return null;
     }
 
     @GetMapping("train/{trainId}")
-    public Response<TrainVO> getTrain(@PathVariable String trainId) {
+    public Response<TrainVO> getTrain(@PathVariable Integer trainId) {
         return Response.success(new TrainVO(1L,"1","1","1",new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()),false));
     }
 }
