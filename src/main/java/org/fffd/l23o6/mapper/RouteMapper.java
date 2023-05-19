d1b2f3541408f232f6a4820001e58e1e652724d1
Bob Huang <bc3104hzy@126.com>
2023-05-19 16:37:14 +0800

    Remove unused imports

--- src/main/java/org/fffd/l23o6/mapper/RouteMapper.java
+++ src/main/java/org/fffd/l23o6/mapper/RouteMapper.java
@@ -1,9 +1,7 @@
 package org.fffd.l23o6.mapper;
 
 import org.fffd.l23o6.pojo.entity.RouteEntity;
-import org.fffd.l23o6.pojo.entity.StationEntity;
 import org.fffd.l23o6.pojo.vo.route.RouteVO;
-import org.fffd.l23o6.pojo.vo.station.StationVO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
--- src/main/java/org/fffd/l23o6/pojo/vo/route/RouteVO.java
+++ src/main/java/org/fffd/l23o6/pojo/vo/route/RouteVO.java
@@ -1,8 +1,5 @@
 package org.fffd.l23o6.pojo.vo.route;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
