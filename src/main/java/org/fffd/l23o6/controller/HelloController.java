8e41537558ea39b30a098c246387edc384a97c2e
Bob Huang <bc3104hzy@126.com>
2023-05-20 18:46:02 +0800

    Forgot to save one file.

--- src/main/java/org/fffd/l23o6/controller/HelloController.java
+++ src/main/java/org/fffd/l23o6/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.fffd.l23o6.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
