bc9cd764b4a4b86de9f17d69d0bf10499bb5426e
Bob Huang <bc3104hzy@126.com>
2023-05-21 22:26:07 +0800

    add NoSuchElementException

--- src/main/java/org/fffd/l23o6/controller/BusinessExceptionHandler.java
+++ src/main/java/org/fffd/l23o6/controller/BusinessExceptionHandler.java
@@ -4,6 +4,9 @@ import cn.dev33.satoken.exception.NotLoginException;
 import io.github.lyc8503.spring.starter.incantation.exception.CommonErrorType;
 import io.github.lyc8503.spring.starter.incantation.pojo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.NoSuchElementException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +17,11 @@ public class BusinessExceptionHandler {
     public CommonResponse<?> handleAuthorizeException(NotLoginException e) {
         log.error("Not Login Exception", e);
         return CommonResponse.error(CommonErrorType.UNAUTHORIZED, e.getMessage());
+    }
+    
+    @ExceptionHandler(NoSuchElementException.class)
+    public CommonResponse<?> handleNoSuchElementException(NoSuchElementException e) {
+        log.error("No Such Element Exception", e);
+        return CommonResponse.error(CommonErrorType.ILLEGAL_ARGUMENTS, "请求的资源不存在");
     }
 }
