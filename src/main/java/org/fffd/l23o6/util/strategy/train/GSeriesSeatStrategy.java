ed35b3596a0f90c491b4c4291cc831bc422e2bad
Bob Huang <bc3104hzy@126.com>
2023-05-23 10:54:57 +0800

    changed @Nullable to jakarta

--- src/main/java/org/fffd/l23o6/util/strategy/train/GSeriesSeatStrategy.java
+++ src/main/java/org/fffd/l23o6/util/strategy/train/GSeriesSeatStrategy.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 
 
 public class GSeriesSeatStrategy extends TrainSeatStrategy {
